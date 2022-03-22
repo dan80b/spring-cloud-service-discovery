03835c34061eae8ab9b730aa71187edb6888e10e
Dan Bejinariu <dan80b@yahoo.com>
2022-03-22 10:06:22 +0200

    introduce eureka discovery client

--- order-service/src/main/java/com/softvision/demo/spring/cloud/orderservice/CustomerServiceProxy.java
+++ order-service/src/main/java/com/softvision/demo/spring/cloud/orderservice/CustomerServiceProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", url = "${customer.service.url}")
+@FeignClient(name = "customer-service")
 public interface CustomerServiceProxy {
 
     @GetMapping("/customers/{id}")
--- order-service/src/main/java/com/softvision/demo/spring/cloud/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/softvision/demo/spring/cloud/orderservice/OrderServiceApplication.java
@@ -2,9 +2,11 @@ package com.softvision.demo.spring.cloud.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.softvision.demo.spring.cloud.orderservice")
 public class OrderServiceApplication {
 
